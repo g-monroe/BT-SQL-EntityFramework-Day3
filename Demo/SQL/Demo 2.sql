7bdb2da474311862465e3e4575db2552a98dccba
Dan Drews <dan.drews@buildertrend.com>
2019-05-16 09:41:50 -0500

    Demo 2

---
+++ Demo/SQL/Demo 2.sql
@@ -0,0 +1,24 @@
+INSERT INTO todos values(1, 'Do this', GETDATE(), 'Description', null, 0)
+INSERT INTO todos values(2, 'Do that', GETDATE(), 'Description', null, 0)
+
+select * from ToDos
+
+Select * from ToDos
+where ToDoId = 1
+
+Select ToDoId, Topic from ToDos
+where Description = 'Description'
+
+select count(1) CountOfTodsINThisTopic, Topic from todos
+Group By Topic
+
+update ToDos
+set Description = null,
+DueDate = GETUTCDATE(),
+Topic = 'Do This Now'
+WHERE TodoId = 2
+
+select * from todos
+
+delete from todos where ToDoId = 2
+delete from todos where ToDoId = 1
